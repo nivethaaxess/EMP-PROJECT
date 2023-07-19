26a04c9547429bca3a3618c67de3cbfb05d7e3c3
nivethaaxess <135101423+nivethaaxess@users.noreply.github.com>
2023-07-19 13:11:18 +0530

    Adding files

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,13 +1,17 @@
 import Admin_Dash from "./Admin Dashboard/Admin_Dash";
-import User from "./component/user"
+import MenuBar from "./Header/Menu";
+import NavBar from "./Header/Nav";
+
 
 function App() {
   return (
     <div className="App">
-        {/* <Admin_Dash/> */}
-        <User/>
+      <MenuBar />
+      <Admin_Dash />
+      <NavBar />
     </div>
   );
 }
 
 export default App;
+
