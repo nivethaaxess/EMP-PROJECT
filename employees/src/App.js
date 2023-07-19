1fed9485462ffb9ddd58909fd258768f4e061c43
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-07-19 12:30:47 +0530

    update

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,10 +1,11 @@
 import Admin_Dash from "./Admin Dashboard/Admin_Dash";
-
+import User from "./component/user"
 
 function App() {
   return (
     <div className="App">
-        <Admin_Dash/>
+        {/* <Admin_Dash/> */}
+        <User/>
     </div>
   );
 }
