eab74597ed2001fc78cb4094301575444dd412cd
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-07-20 12:31:25 +0530

    checkbox,Grid update

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,6 +1,6 @@
 import Admin_Dash from "./Admin Dashboard/Admin_Dash";
-import User from "./component/user"
-
+ import User from "./component/user"
+// import User from "./component/nive"
 function App() {
   return (
     <div className="App">
