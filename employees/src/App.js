d151663e16bbe47d7b6783a1c22a031222c0d295
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-07-28 14:50:32 +0530

    accc change

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,6 +1,6 @@
 import Admin_Dash from "./Admin Dashboard/Admin_Dash";
-  import User from "./component/user"
-  //import User from "./component/nive"
+ // import User from "./component/user"
+  import User from "./component/nive"
 
 import MenuBar from "./Header/MenuBar";
 import NavBar from "./Header/NavBar";
