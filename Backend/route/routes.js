8decadb90784c62039d7bd2c864199f956fc29a8
nivetha <135101423+nivethaaxess@users.noreply.github.com>
2023-08-03 13:00:24 +0530

    employeedata updated

--- Backend/route/routes.js
+++ Backend/route/routes.js
@@ -1,58 +1,23 @@
 const router = require('express').Router();
 
-console.log('CHECK')
-
-
-
 // dinesh-----------
 
 const Dashboard = require('../Dashboard Data/Dashboard');
- router.get('/api/Dashboard',Dashboard.DashboardData);
 
+    router.get('/api/Dashboard',Dashboard.DashboardData);
+    router.post('/api/table/data',Dashboard.tableData);
 
-router.post('/api/table/data',Dashboard.tableData);
+ // employeeData starts -----------
 
+const employeeData = require("../EmployeeData/employeeData")
 
- // nivetha starts -----------
-const nivetha = require("../nivethaData/niviData")
-const employeeData = require("../nivethaData/employeeData")
+    router.get('/courseList/:id',employeeData.userCourseList)
+    router.get('/subTopicCount',employeeData.subTopicCount)
+    router.post('/updatestatus',employeeData.updatestatus)
+    router.post('/insertusers',employeeData.InsertUser)
+    router.get('/api/getdata',employeeData.getdata);
 
-
-router.get('/domainTable',nivetha.domainTable)
-router.get('/usersTable',nivetha.UsersTable)
-router.get('/topicsTable',nivetha.topicsTable)
-router.get('/subTopicsTable',nivetha.subTopicsTable)
-router.get('/statusTable',nivetha.statusTable)
-
-
-router.post('/InsertDomain',nivetha.InsertDomain)
-router.post('/insertusers',nivetha.InsertUser)
-router.post('/insertTopics',nivetha.InsertTopics)
-router.post('/insertSubTopics',nivetha.InsertSubTopics)
-router.post('/updatestatus',nivetha.updatestatus)
-
-
-router.get('/courseList/:id',employeeData.userCourseList)
-router.get('/subTopicCount',employeeData.subTopicCount)
-router.get('/completedCount',employeeData.completedCount)
-
-
-
- // nivetha ends -----------
-
-
-
- //sai starts---
-
- const backend = require("../nivethaData/sai")
- router.get('/api/getdata',backend.getdata);
- 
-//  router.post('/api/updatestatus',backend.updatestatus);
-//  router.post('/api/post',backend.NotcompletedPostdata);
-
- // sai ends------------
-
-
+ // employeeData ends -----------
 
 
 
