9067e01894f1a0fe943354cdb696484658c0cdec
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2023-08-09 10:52:16 +0530

    Mysql error fixed

--- Backend/Mysql connect/Mysql.js
+++ Backend/Mysql connect/Mysql.js
@@ -1,50 +1,40 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'root', /* MySQL User */
-    // password: '', /* MySQL Password */
-    // database: 'employeedata' /* MySQL Database */    
+const connection = mysql.createPool({
+  connectionLimit: 10,
+  host: 'srv787.hstgr.io',
+  user: 'u842521168_new',
+  password: 'Admin@123',
+  database: 'u842521168_new',
+  waitForConnections: true,
+  queueLimit: 0,
+});
 
-///////////////////////////////////////////////////////////     
-connectionLimit: 10,
-    host: 'srv787.hstgr.io',
-    user: 'u842521168_new', /* MySQL User */ 
-    password: 'Admin@123', /* MySQL Password */
-    // database: 'employeedata' 
-    database: 'u842521168_new' ,  /* MySQL Database */
-    waitForConnections: true,      
-  queueLimit: 0
-     
+// Handle MySQL connection errors using an error event listener
+function handleConnectionErrors() {
+  connection.on('error', (err) => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('MySQL Connection Lost. Reconnecting...');
+      connection.query('SELECT 1', (error) => { 
+        if (error) {
+          console.error('Error reconnecting:', error);
+        } else {
+          console.log('Reconnected to MySQL successfully!');
+        }
+      });
+    } else {
+      console.error('MySQL Connection Error:', err); 
+      // Handle other connection errors gracefully
+    }
   });
+}
 
+// Initialize the MySQL connection and error handling
+function initConnection() {
+  handleConnectionErrors();
+  // Perform database operations here
+}
 
-  function handleDisconnect() {
-    connection.connect((err) => {
-      if (err) {       
-        console.error('Error connecting to MySQL:', err);
-        // Retry the connection after a short delay
-        setTimeout(handleDisconnect, 200000);
-      } else {                             
-        console.log('Connected to MySQL successfully!'); 
-      }
-    });          
-  
-    connection.on('error', (err) => {
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        console.error('MySQL Connection Lost. Reconnecting...');    
-        handleDisconnect();
-      } else {     
-        console.error('MySQL Connection Error:', err);
-        // For other errors, you might choose to handle them differently
-        // or terminate the application gracefully.
-      }    
-    });  
-  }
-  
-  handleDisconnect();
-  
+initConnection(); // Start the connection initialization process
 
-
-  
-  module.exports = connection;
+module.exports = connection;
